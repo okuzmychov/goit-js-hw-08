1ba39ef104ea74a099dca403e71d191b3ca5f598
okuzmychov <125366168+okuzmychov@users.noreply.github.com>
2023-07-01 21:05:51 +0000

    Deploying to gh-pages from @ okuzmychov/goit-js-hw-08@9332601444e1180bb785ee8f45c51e17a41969be 🚀

--- 03-feedback.2a957910.js
+++
@@ -1,2 +0,0 @@
-form.addEventListener("input",(function(e){t[e.target.name]=e.target.value}));const t={};
-//# sourceMappingURL=03-feedback.2a957910.js.map
--- 03-feedback.6a297060.js
+++
@@ -1,2 +0,0 @@
-!function(){form.addEventListener("input",(function(n){t[n.target.name]=n.target.value}));var t={}}();
-//# sourceMappingURL=03-feedback.6a297060.js.map
